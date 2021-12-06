248e93e6c1257857715338358da5b7519b722d0b
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2021-12-06 12:31:17 +0100

    Finished second exercise

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,22 +1,120 @@
+using System;
+using System.Collections.Generic;
+
 namespace ComplexAlgebra
 {
     /// <summary>
     /// A type for representing Complex numbers.
     /// </summary>
     ///
-    /// TODO: Model Complex numbers in an object-oriented way and implement this class.
-    /// TODO: In other words, you must provide a means for:
-    /// TODO: * instantiating complex numbers
-    /// TODO: * accessing a complex number's real, and imaginary parts
-    /// TODO: * accessing a complex number's modulus, and phase
-    /// TODO: * complementing a complex number
-    /// TODO: * summing up or subtracting two complex numbers
-    /// TODO: * representing a complex number as a string or the form Re +/- iIm
-    /// TODO:     - e.g. via the ToString() method
-    /// TODO: * checking whether two complex numbers are equal or not
-    /// TODO:     - e.g. via the Equals(object) method
+    /// Model Complex numbers in an object-oriented way and implement this class.
+    /// In other words, you must provide a means for:
+    ///* instantiating complex numbers
+    ///* accessing a complex number's real, and imaginary parts
+    ///* accessing a complex number's modulus, and phase
+    ///* complementing a complex number
+    ///* summing up or subtracting two complex numbers
+    ///* representing a complex number as a string or the form Re +/- iIm
+    ///- e.g. via the ToString() method
+    ///* checking whether two complex numbers are equal or not
+    ///- e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        //private double _real;
+        //private double _imm;
+
+        public double Real
+        {
+            get;
+        }
+
+        public double Imaginary
+        {
+            get;
+        }
+
+        public Complex(double real, double imm)
+        {
+            Real = real;
+            Imaginary = imm;
+        }
+
+        public double Modulus
+        {
+            get => System.Math.Sqrt(Real * Real + Imaginary * Imaginary);
+        }
+
+        public double Phase
+        {
+            get => System.Math.Atan2(Imaginary, Real);
+        }
+
+        public Complex Complement()
+        {
+            return new Complex(Real, -Imaginary);
+        }
+
+        public Complex Plus(Complex c)
+        {
+            return new Complex(Real + c.Real, Imaginary + c.Imaginary);
+        }
+        public Complex Minus(Complex c)
+        {
+            return new Complex(Real - c.Real, Imaginary - c.Imaginary);
+        }
+
+        public override string ToString()
+        {
+          
+            string n="";
+            if (Real > 0)
+            {
+                n = "+" + Real.ToString();
+            }
+            else if (Real < 0)
+            {
+                n = Real.ToString();
+            }
+            else
+            {
+                //Real=0
+            }
+
+
+            if (Imaginary * Imaginary == 1)
+            {
+                n = n + (Imaginary == 1 ? "+i" : "-i");
+            }
+            else if (Imaginary > 0)
+            {
+                n = n + "+" + Imaginary.ToString();
+            }
+            else if (Imaginary < 0)
+            {
+                n = n + Imaginary.ToString();
+            }
+            else
+            {
+                //Immaginary=0
+            }
+
+            if (Real == 0 && Imaginary == 0) return "0";
+
+            return n;
+        }
+
+
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Real, Imaginary);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Complex complex &&
+                   Real == complex.Real &&
+                   Imaginary == complex.Imaginary;
+        }
     }
 }
